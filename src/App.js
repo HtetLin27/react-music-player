951730167a4d8c187e1b70d6e41018970e2dd63f
htetlynn <htetlynnko27@gmail.com>
2024-10-28 01:32:35 +0700

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+ 
+ import './styles/app.scss'
+
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
   return (
     <div className="App">
-      APP
+      <Song/>
+      <Player/>
     </div>
   );
 }
---
+++ src/components/Song.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Song = () => {
+  return (
+    <div className='song-container'>
+        <div>
+            Picture
+        </div>
+        <div>
+            Song Name
+        </div>
+        <div>
+            Artist
+        </div>
+    </div>
+  )
+}
+
+export default Song
